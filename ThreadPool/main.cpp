ded696f67e085a61f6d73c3bacb729e17d3a20ea
Pengxin <slive1024@hotmail.com>
2022-06-24 19:54:25 +0800

    ThreadPool submit task function complete

--- ThreadPool/main.cpp
+++ ThreadPool/main.cpp
@@ -4,11 +4,24 @@
 
 #include "threadpool.h"
 
+class MyTask : public Task {
+ public:
+  void run() { 
+	std::cout << "tid: " << std::this_thread::get_id() << "begin"<<std::endl;
+	std::this_thread::sleep_for(std::chrono::seconds(2));
+  }
+};
+
 int main() { 
   ThreadPool pool; 
-  pool.start();
+  pool.start(4);
 
-  std::this_thread::sleep_for(std::chrono::seconds(5));
+  pool.submitTask(std::make_shared<MyTask>());
+  pool.submitTask(std::make_shared<MyTask>());
+  pool.submitTask(std::make_shared<MyTask>());
+  pool.submitTask(std::make_shared<MyTask>());
+
+  getchar();
    
 }
 
